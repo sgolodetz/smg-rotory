adf3db9c93af5fb1b5a574ed26d21672e0608866
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-06-28 13:20:33 +0100

    Get basic beacon localisation working.

--- smg/rotory/beacons/beacon_localiser.py
+++ smg/rotory/beacons/beacon_localiser.py
@@ -1,4 +1,6 @@
+import math
 import numpy as np
+import scipy.optimize
 
 from collections import defaultdict
 from typing import Dict, List, Optional, Tuple
@@ -7,7 +9,11 @@ from .beacon import Beacon
 
 
 class BeaconLocaliser:
-    """TODO"""
+    """
+    TODO
+
+    See also: https://www.alanzucconi.com/2017/03/13/positioning-and-trilateration/
+    """
 
     # CONSTRUCTOR
 
@@ -50,6 +56,30 @@ class BeaconLocaliser:
     # PUBLIC STATIC METHODS
 
     @staticmethod
+    def mean_square_error(beacon_pos: np.ndarray, beacon_measurements: List[Tuple[np.ndarray, float]]) -> float:
+        # TODO
+        mse: float = 0.0
+        for receiver_pos, measured_distance in beacon_measurements:
+            calculated_distance: float = np.linalg.norm(receiver_pos - beacon_pos)
+            mse += math.pow(calculated_distance - measured_distance, 2.0)
+
+        return mse / len(beacon_measurements)
+
+    @staticmethod
     def try_localise_beacon(beacon_measurements: List[Tuple[np.ndarray, float]]) -> Optional[np.ndarray]:
         # TODO
-        pass
+        if len(beacon_measurements) < 3:
+            return None
+
+        result: scipy.optimize.optimize.OptimizeResult = scipy.optimize.minimize(
+            BeaconLocaliser.mean_square_error,
+            np.zeros(3),
+            args=beacon_measurements,
+            method="L-BFGS-B",
+            options={
+                "ftol": 1e-5,
+                "maxiter": 1e+7
+            }
+        )
+
+        return result.x
